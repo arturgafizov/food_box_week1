6546c912a0dc2db058dffb9b7e7bad1a9bd5d51e
Artur <art05xxx@rambler.ru>
2021-01-31 20:02:11 +0300

    week2 part 2

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from users.views import UserList
 from users.views import CurrentUserRetrieveUpdateView
-from users.views import auth_token
 
 
 urlpatterns_users = [
     path('auth/register/', UserList.as_view(), name='UserList'),  # local item url created
-    path('auth/login/', auth_token),
+    path('auth/login/', obtain_auth_token),
     path('current/', CurrentUserRetrieveUpdateView.as_view(), name='CurrentUser'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,6 @@
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.generics import RetrieveUpdateAPIView, CreateAPIView
-from rest_framework.permissions import AllowAny
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from django.contrib.auth import authenticate
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authtoken.models import Token
-from rest_framework.status import (
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-    HTTP_200_OK
-)
+
 
 from users.models import User
 from users.models import UserSerializer, UserCurrentSerializer
@@ -24,21 +14,6 @@ class UserList(CreateAPIView):
 class UserRegisterViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-@ csrf_exempt
-@api_view(["POST"])
-@permission_classes((AllowAny,))
-def auth_token(request):
-    username = request.data.get("username")
-    password = request.data.get("password")
-    if username is None or password is None:
-        return Response({'error': 'Please provide both username and password'}, status=HTTP_400_BAD_REQUEST)
-    user = authenticate(username=username, password=password)
-    if not user:
-        return Response({'error': 'Invalid Credentials'}, status=HTTP_404_NOT_FOUND)
-    token, _ = Token.objects.get_or_create(user=user)
-    return Response({'token': token.key}, status=HTTP_200_OK)
 
 
 class CurrentUserRetrieveUpdateView(RetrieveUpdateAPIView):
